be8c7e74bc9b7927c617bb1f0a6fe587d4a6763f
anhtv4 <tva8320@gmail.com>
2023-10-12 17:17:46 +0700

    add feature upload image for company register

--- FrontEnd/src/app/modules/recruiter/components/company-register/company-register.component.ts
+++ FrontEnd/src/app/modules/recruiter/components/company-register/company-register.component.ts
@@ -8,6 +8,10 @@ import { FormControl, Validators } from '@angular/forms';
   styleUrls: ['./company-register.component.css']
 })
 export class CompanyRegisterComponent {
+  //upload img
+  displayImage = "none"
+  fileSrc: any;
+
   title = 'angular';
   public Editor = ClassicEditor;
 
@@ -19,68 +23,82 @@ export class CompanyRegisterComponent {
   addressRq = new FormControl('', [Validators.required]);
   phoneRq = new FormControl('', [Validators.required]);
 
-  getErrorMessageName(){
-    if(this.nameRq.hasError('required')){
+  getErrorMessageName() {
+    if (this.nameRq.hasError('required')) {
       return 'Tên công ty không được để trống!'
     }
     return
   }
-  getErrorMessageEmail(){
-    if(this.emailRq.hasError('required')){
+  getErrorMessageEmail() {
+    if (this.emailRq.hasError('required')) {
       return 'Email không được để trống!'
     }
-    if(this.emailRq.hasError('email')){
+    if (this.emailRq.hasError('email')) {
       return 'Email không hợp lệ!'
     }
     return
   }
-  getErrorMessageTaxNum(){
-    if(this.taxNumRq.hasError('required')){
+  getErrorMessageTaxNum() {
+    if (this.taxNumRq.hasError('required')) {
       return 'Mã số thuế không được để trống!'
     }
     return
   }
-  getErrorMessageIndustry(){
-    if(this.industryRq.hasError('required')){
+  getErrorMessageIndustry() {
+    if (this.industryRq.hasError('required')) {
       return 'Lĩnh vực không được để trống!'
     }
-    if(this.industryRq.hasError('min')){
+    if (this.industryRq.hasError('min')) {
       return 'Lĩnh vực không được để trống!'
     }
     return
   }
-  getErrorMessageSize(){
-    if(this.sizeRq.hasError('required')){
+  getErrorMessageSize() {
+    if (this.sizeRq.hasError('required')) {
       return 'Quy mô không được để trống!'
     }
-    if(this.sizeRq.hasError('min')){
+    if (this.sizeRq.hasError('min')) {
       return 'Quy mô không được để trống!'
     }
     return
   }
-  getErrorMessageAddress(){
-    if(this.addressRq.hasError('required')){
+  getErrorMessageAddress() {
+    if (this.addressRq.hasError('required')) {
       return 'Địa chỉ không được để trống!'
     }
     return
   }
-  getErrorMessagePhone(){
-    if(this.phoneRq.hasError('required')){
+  getErrorMessagePhone() {
+    if (this.phoneRq.hasError('required')) {
       return 'Số điện thoại không được để trống!'
     }
     return
   }
 
-  submitButtonClicked(){
-      this.nameRq.markAllAsTouched()
-      this.emailRq.markAllAsTouched()
-      this.taxNumRq.markAllAsTouched()
-      this.industryRq.markAllAsTouched()
-      this.sizeRq.markAllAsTouched()
-      this.addressRq.markAllAsTouched()
-      this.phoneRq.markAllAsTouched()
-  
-      console.log('submit button clicked')
-      return
+  submitButtonClicked() {
+    this.nameRq.markAllAsTouched()
+    this.emailRq.markAllAsTouched()
+    this.taxNumRq.markAllAsTouched()
+    this.industryRq.markAllAsTouched()
+    this.sizeRq.markAllAsTouched()
+    this.addressRq.markAllAsTouched()
+    this.phoneRq.markAllAsTouched()
+
+    console.log('submit button clicked')
+    return
+  }
+
+  getFile(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.displayImage = "block"
+
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => {
+        this.fileSrc = event.target?.result;
+      }
+    }
   }
 }
