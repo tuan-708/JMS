4a6ce2960a79d60a531983b5c1d31be5c91b08d8
tuan-708 <tuanlbs78@gmail.com>
2023-10-27 07:07:38 +0700

    <Tuan> Slider Home Page

--- FrontEnd/src/app/modules/candidate/components/sliders/sliders.component.ts
+++ FrontEnd/src/app/modules/candidate/components/sliders/sliders.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { environment } from 'src/environments/environment';
+declare var $: any; // Declare $ as a global variable
 
 @Component({
   selector: 'candidate-sliders',
@@ -8,4 +9,27 @@ import { environment } from 'src/environments/environment';
 })
 export class SlidersComponent {
   apiURL = environment.apiUrl;
+
+  ngAfterViewInit() {
+
+    $(document).ready(function () {
+      $('#myCarousel').carousel({
+        interval: 3000 // Change the value (in milliseconds) to adjust the time interval
+      });
+    });
+
+    $('#prev').on('click', function () {
+      $('#cards').animate({
+        scrollLeft: '-=250'
+      }, 500, 'swing');
+    });
+
+    $('#next').on('click', function () {
+      $('#cards').animate({
+        scrollLeft: '+=250'
+      }, 500, 'swing');
+    });
+
+  }
+
 }
