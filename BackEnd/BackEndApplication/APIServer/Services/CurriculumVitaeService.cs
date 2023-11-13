a93e4fe6ca4368d661c02db3210be819c9890f2a
DangThanh2001 <thanhsunflower2001@gmail.com>
2023-11-13 14:35:49 +0700

    add update cv api

--- BackEnd/BackEndApplication/APIServer/Services/CurriculumVitaeService.cs
+++ BackEnd/BackEndApplication/APIServer/Services/CurriculumVitaeService.cs
@@ -6,6 +6,7 @@ using APIServer.IServices;
 using APIServer.Models;
 using APIServer.Models.Entity;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using X.PagedList;
 
@@ -17,13 +18,15 @@ namespace APIServer.Services
         private readonly ICVMatchingRepository _CVApplyContext;
         private readonly IMapper _mapper;
         private readonly IConfiguration _configuration;
+        private readonly ICandidateRepository _candidateRepository;
 
-        public CurriculumVitaeService(ICurriculumVitaeRepository context, ICVMatchingRepository CVApplyContext, IMapper mapper, IConfiguration configuration)
+        public CurriculumVitaeService(ICurriculumVitaeRepository context, ICVMatchingRepository CVApplyContext, IMapper mapper, IConfiguration configuration, ICandidateRepository candidateRepository)
         {
             _context = context;
             _CVApplyContext = CVApplyContext;
             _mapper = mapper;
             _configuration = configuration;
+            _candidateRepository = candidateRepository;
         }
 
         public int Create(CurriculumVitae data)
@@ -89,5 +92,81 @@ namespace APIServer.Services
             throw new NotImplementedException();
         }
 
+        public int UpdateCvByCandidateIdAndCvId(int candidateId, int cvId, CurriculumVitaeDTO cvDTO)
+        {
+            var can = _candidateRepository.GetById(candidateId);
+            var cv = _context.GetById(cvId);
+            if (can == null || cv == null || cvDTO == null)
+            {
+                throw new Exception("Data not valid");
+            }
+            if (cv.CandidateId != candidateId)
+            {
+                throw new Exception("Permission denied");
+            }
+            var cvChange = _mapper.Map<CurriculumVitae>(cvDTO);
+            using (var context = new JMSDBContext())
+            {
+                var cvOrigin = context.CurriculumVitaes
+                    .Include(x => x.Category)
+                    .Include(x => x.Educations)
+                    .Include(x => x.Certificates)
+                    .Include(x => x.Awards)
+                    .Include(x => x.Skills)
+                    .Include(x => x.Projects)
+                    .Include(x => x.JobExperiences)
+                    .Include(x => x.Gender)
+                    .Include(x => x.Level)
+                    .Include(x => x.EmploymentType)
+                    .First(x => x.Id == cvId);
+                if (cvOrigin.Educations.Any())
+                {
+                    context.RemoveRange(cvOrigin.Educations);
+                }
+                if (cvOrigin.Certificates.Any())
+                {
+                    context.RemoveRange(cvOrigin.Certificates);
+                }
+                if (cvOrigin.Awards.Any())
+                {
+                    context.RemoveRange(cvOrigin.Awards);
+                }
+                if (cvOrigin.Skills.Any())
+                {
+                    context.RemoveRange(cvOrigin.Skills);
+                }
+                if (cvOrigin.Projects.Any())
+                {
+                    context.RemoveRange(cvOrigin.Projects);
+                }
+                if (cvOrigin.JobExperiences.Any())
+                {
+                    context.RemoveRange(cvOrigin.JobExperiences);
+                }
+
+                cvOrigin.CareerGoal = cvChange.CareerGoal;
+                cvOrigin.EmploymentTypeId = cvChange.EmploymentTypeId;
+                cvOrigin.Phone = cvChange.Phone;
+                cvOrigin.DisplayName = cvChange.DisplayName;
+                cvOrigin.DisplayEmail = cvChange.DisplayEmail;
+                cvOrigin.Address = cvChange.Address;
+                cvOrigin.DOB = cvChange.DOB;
+                cvOrigin.LevelId = cvChange.LevelId;
+                cvOrigin.CategoryId = cvChange.CategoryId;
+                cvOrigin.CVTitle = cvChange.CVTitle;
+                cvOrigin.Font = cvChange.Font;
+                cvOrigin.Theme = cvChange.Theme;
+                cvOrigin.GenderId = cvChange.GenderId; 
+
+                cvOrigin.Educations = cvChange.Educations;
+                cvOrigin.Certificates = cvChange.Certificates;
+                cvOrigin.Awards = cvChange.Awards;
+                cvOrigin.Skills = cvChange.Skills;
+                cvOrigin.Projects = cvChange.Projects;
+                cvOrigin.JobExperiences = cvChange.JobExperiences;
+
+                return context.SaveChanges();
+            }
+        }
     }
 }
