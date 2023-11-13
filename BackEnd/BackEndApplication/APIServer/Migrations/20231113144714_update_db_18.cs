03ef7167a1512b7e9d458737931502c5a09fdc7c
DangThanh2001 <thanhsunflower2001@gmail.com>
2023-11-13 22:17:39 +0700

    update api recruiter

---
+++ BackEnd/BackEndApplication/APIServer/Migrations/20231113144714_update_db_18.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIServer.Migrations
+{
+    public partial class update_db_18 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_RecuirterId",
+                table: "Companies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_RecuirterId",
+                table: "Companies",
+                column: "RecuirterId",
+                unique: true,
+                filter: "[RecuirterId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_RecuirterId",
+                table: "Companies");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_RecuirterId",
+                table: "Companies",
+                column: "RecuirterId");
+        }
+    }
+}
