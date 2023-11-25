1e8c6e3f4fd2bf0a78dba4addc2246ede205485c
anhtv4 <tva8320@gmail.com>
2023-11-25 16:02:43 +0700

    update func

--- FrontEnd/src/app/modules/admin/admin.component.ts
+++ FrontEnd/src/app/modules/admin/admin.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+  routerPath: any
 
+  constructor(private router: Router) {
+    this.routerPath = this.router.url
+  }
 }
