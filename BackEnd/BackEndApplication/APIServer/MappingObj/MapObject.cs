a38065fcc4ebf9c9ec840349095dfcc3a7b2218c
DangThanh2001 <thanhsunflower2001@gmail.com>
2023-11-26 20:01:08 +0700

    fix avt url

--- BackEnd/BackEndApplication/APIServer/MappingObj/MapObject.cs
+++ BackEnd/BackEndApplication/APIServer/MappingObj/MapObject.cs
@@ -78,7 +78,9 @@ namespace APIServer.MappingObj
                 .ForMember(x => x.LastUpdateDate, src => src.MapFrom(src => src.LastUpdateDate.ToString(GlobalStrings.FORMAT_DATE1)))
                 .ForMember(x => x.ApplyDate, src => src.MapFrom(src => src.ApplyDate.ToString(GlobalStrings.FORMAT_DATE1)))
                 .ForMember(x => x.CreatedDate, src => src.MapFrom(src => src.CreatedDate.ToString(GlobalStrings.FORMAT_DATE1)))
-                .ForMember(x => x.AvatarURL, src => src.MapFrom(src => host + src.AvatarURL ))
+                .ForMember(x => x.AvatarURL, src => src.MapFrom(src =>  Validation.checkStringIsEmpty(src.AvatarURL) ?
+                host + "\\defaults\\default_avt.jpg" :
+                host + src.AvatarURL ))
                 ;
 
             CreateMap<Candidate, CandidateDTO>()
